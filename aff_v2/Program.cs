131264c21d5925dc3ce6ce5e59689b1b77ce4744
diamondbo <diamondboy34@gmail.com>
2026-01-21 18:57:14 +0000

    Initial commit with settings now got to add and test services

---
+++ aff_v2/Program.cs
@@ -0,0 +1,33 @@
+using aff_v2.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.Configure<AlpacaSettings>(builder.Configuration.GetSection("AlpacaInfo"));
+builder.Services.AddScoped<IAlpacaService, AlpacaService>();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
