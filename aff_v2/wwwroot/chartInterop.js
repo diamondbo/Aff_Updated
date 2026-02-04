8a1217d0e61f60879d6c12daf9817342cde56ada
diamondbo <diamondboy34@gmail.com>
2026-02-04 17:27:58 +0000

    added graph and still strugling with ignoring test bin and obj

---
+++ aff_v2/wwwroot/chartInterop.js
@@ -0,0 +1,14 @@
+window.chartInterop = {
+
+    charts: {},
+
+    createChart: function (canvasId, config) {
+        const ctx = document.getElementById(canvasId);
+        if (!ctx) return;
+        if (this.charts[canvasId]){
+            this.charts[canvasId].destroy();
+        }
+
+        this.charts[canvasId] = new Chart(ctx, config);
+    }
+};
